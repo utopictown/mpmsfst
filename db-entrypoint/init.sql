a9cec37e4e5ffb319e46badbdad649c5d011d73f
utopictown <ekoprstyo.mail@gmail.com>
2022-12-10 02:37:00 +0700

    build: :hammer: add initial data

--- db-entrypoint/init.sql
+++ db-entrypoint/init.sql
@@ -1,2 +1,6 @@
 SET CHARSET UTF8;
 CREATE DATABASE mpmsfst DEFAULT CHARACTER SET utf8;
+
+INSERT INTO `mpmsfst`.`user` (id, username, password) VALUES (`67032988-77f2-11ed-8dea-0242ac1b0002`, `test`, `test`);
+
+INSERT INTO `mpmsfst`.`client` (id, username, password) VALUES (`99b7f4c5-77f2-11ed-8dea-0242ac1b0002`, `clientsecret`, `http://localhost/callback`);
